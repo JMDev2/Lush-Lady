e7a9e2b956a598aacd5d6eb9d20f1a8937a0762f
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-03-22 20:47:24 +0300

    ft:This will increase the heigh of the corousel images

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
-    $(".skincare5").mouseover(function(){
+    $(".skincare5").hover(function(){
         $(".cart5").slideDown();
     });
+    $(".skincare5").mouseout(function(){
+        $(".cart5").slideUp();
+    });
+    $(".skincare4").hover(function(){
+        $(".cart4").slideDown();
+    });
+    $(".skincare4").mouseout(function(){
+        $(".cart4").slideUp();
+    });
+    
+    $(".skincare3").hover(function(){
+        $(".cart3").slideDown();
+    });
+    $(".skincare3").mouseout(function(){
+        $(".cart3").slideUp();
+    });
+    $(".skincare2").hover(function(){
+        $(".cart2").slideDown();
+    });
+    $(".skincare2").mouseout(function(){
+        $(".cart2").slideUp();
+    });
+    $(".skincare1").hover(function(){
+        $(".cart1").slideDown();
+    });
+    $(".skincare1").mouseout(function(){
+        $(".cart1").slideUp();
+    });
+    // $(".skincare11").mouseover(function(){
+    //     $(".cart11").slideDown();
+    // });
+    // $(".skincare11").mouseout(function(){
+    //     $(".cart11").slideUp();
+    // });
+    // $(".skincare22").mouseover(function(){
+    //     $(".cart22").slideDown();
+    // });
+    // $(".skincare22").mouseout(function(){
+    //     $(".cart22").slideUp();
+    // });
+
 })
