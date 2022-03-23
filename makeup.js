ab6a77feaf6e6e5b8f2338bbfadf619271d41024
Karenkoech <karenkoech3@gmail.com>
2022-03-23 17:32:31 -0400

    input onclick function and give it functionality

--- makeup.js
+++ makeup.js
@@ -1,8 +1,31 @@
 function myFunction() {
-    var x = document.getElementById("#Img");
+    var x = document.getElementById("prodScope");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x = document.getElementById("proSco");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    var x = document.getElementById("proScope");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
   }
+//   $(document).ready(function(){
+//     $("button,#prodScope").click(function(){
+//         $("button,#prodScope").toggle()
+//     })
+//     $("#icon2,#para2").hover(function(){
+//      $("#icon2,#para2").toggle()
+//  })
+//  $("#icon3,#para3").hover(function(){
+//      $("#icon3,#para3").toggle()
+//  })
+// })
