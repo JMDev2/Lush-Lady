780662f1e7e424d0317d2d5ebb7405fc67d1aefa
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-03-23 08:25:40 +0300

    ft:This will edit the height of section and article

--- index.js
+++ index.js
@@ -1,46 +1,10 @@
-$(document).ready(function(){
-    $(".skincare5").hover(function(){
+$(".card-img-top").hover(function(){
+    $(".card-text").toggle();
+    
+  });
+
+  $(document).ready(function(){
+    $("p").click(function(){
         $(".cart5").slideDown();
     });
-    $(".skincare5").mouseout(function(){
-        $(".cart5").slideUp();
-    });
-    $(".skincare4").hover(function(){
-        $(".cart4").slideDown();
-    });
-    $(".skincare4").mouseout(function(){
-        $(".cart4").slideUp();
-    });
-    
-    $(".skincare3").hover(function(){
-        $(".cart3").slideDown();
-    });
-    $(".skincare3").mouseout(function(){
-        $(".cart3").slideUp();
-    });
-    $(".skincare2").hover(function(){
-        $(".cart2").slideDown();
-    });
-    $(".skincare2").mouseout(function(){
-        $(".cart2").slideUp();
-    });
-    $(".skincare1").hover(function(){
-        $(".cart1").slideDown();
-    });
-    $(".skincare1").mouseout(function(){
-        $(".cart1").slideUp();
-    });
-    // $(".skincare11").mouseover(function(){
-    //     $(".cart11").slideDown();
-    // });
-    // $(".skincare11").mouseout(function(){
-    //     $(".cart11").slideUp();
-    // });
-    // $(".skincare22").mouseover(function(){
-    //     $(".cart22").slideDown();
-    // });
-    // $(".skincare22").mouseout(function(){
-    //     $(".cart22").slideUp();
-    // });
-
 })
