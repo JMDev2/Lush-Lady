7762ec8196ad3738e6573f9d53986dcdbd338072
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-03-23 23:41:34 +0300

    This will remove the comented codes

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
 
 
-  // $(document).ready(function(){
-  //   $(".card-img-top").hover(function(){
-  //     $(".card-text").toggle();
-  //   });
-  // $(document).ready(function(){
-  // $(".card-img-top1").hover(function(){
-  //   $(".card-text").toggle();
-  // });
-  // $(".card-img-top2").hover(function(){
-  //   $(".card-text").toggle();
-  // });
-  // $(".card-img-top3").hover(function(){
-  //   $(".card-text").toggle();
-  // });
-  // $(".card-img-top4").hover(function(){
-  //   $(".card-text").toggle();
-  // });
-
-
-})
+ 
